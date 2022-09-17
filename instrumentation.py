17405fdea0addfe7320e35d4c0ef20146bec98a7
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-09-17 12:51:25 -0700

    Create instrumentation module

---
+++ instrumentation.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+class ThermostatModel:
+    def __init__(self) -> None:
+        pass
+
+    def simulate(self, inputs, interval) -> Any:
+        pass
+
+
+class ThermostatSpecification:
+    def __init__(self) -> None:
+        pass
+
+    def evaluate(self, states, times) -> Any:
+        pass
