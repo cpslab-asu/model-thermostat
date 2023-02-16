ae7dc3cfd65788ba643c39ca9390954f6fbda5dd
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-16 14:03:49 -0700

    Refactor instrumentation entrypoint to show number of samples before full coverage achieved

--- src/instrumentation/__main__.py
+++ src/instrumentation/__main__.py
@@ -14,24 +14,7 @@ options = Options(
 )
 
 result = staliro(model, specification, UniformRandom(), options)
-best_eval = min((eval for run in result.runs for eval in run.history), key=lambda e: e.cost)
-model_output_best = simulate_model(model, options, best_eval.sample)
-most_states_visited = {
-    active_state(specification.kripke, instr_output.variables)
-    for instr_output in model_output_best.trace.states
-}
-
-worst_eval = max((eval for run in result.runs for eval in run.history), key=lambda e: e.cost)
-model_output_worst = simulate_model(model, options, worst_eval.sample)
-fewest_states_visited = {
-    active_state(specification.kripke, instr_output.variables)
-    for instr_output in model_output_worst.trace.states
-}
+run = result.runs[0]
 
 print(f"Function branches: {len(specification.kripke.states)}")
-print("========================")
-print(f"Maximum Distance: {best_eval.cost}")
-print(f"Maximum branches covered: {len(most_states_visited)}")
-print("========================")
-print(f"Minimum Distance: {worst_eval.cost}")
-print(f"Minimum branches covered: {len(fewest_states_visited)}")
+print(f"Coverage achieved in: {len(run.history)}")
