7568fe48355c1467d9a27946c9b6aa06ae63aaeb
Quinn Thibeault <qthibeau@asu.edu>
2023-02-20 11:22:06 -0700

    Create SOAR optimizer to wrap pysoarc optimizer

--- src/instrumentation/optimizer.py
+++ src/instrumentation/optimizer.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
-from typing import Generator, Sequence
+from typing import Generator, Optional, Sequence
 
+import numpy as np
 import numpy.random as rand
+from numpy.typing import NDArray
+from pysoarc import PySOARC
+from pysoarc.coreAlgorithm import Behavior
+from pysoarc.gprInterface import InternalGPR
 from staliro.core import Interval, Sample
 from staliro.core.optimizer import ObjectiveFn, Optimizer
 
@@ -41,3 +46,41 @@ class UniformRandom(Optimizer[SystemCoverage, None]):
             if evaluation.remaining_states == 0:
                 break
 
+class SOAR(Optimizer[SystemCoverage, None]):
+    def optimize(
+        self,
+        func: ObjectiveFn[SystemCoverage],
+        bounds: Sequence[Interval],
+        budget: int,
+        seed: int
+    ) -> None:
+        def _test_fn(input: NDArray[np.double]) -> Optional[tuple[float, float]]:
+            if input.ndim != 1:
+                raise ValueError("Input array must be 1-d")
+
+            sample = Sample(tuple(input.tolist()))
+            cov = func.eval_sample(sample)
+            return None if cov.remaining_states == 0 else cov.hybrid_distance
+
+        _ = PySOARC(
+            n_0=20,
+            nSamples=budget,
+            trs_max_budget=5,
+            max_loc_iter=5,
+            inpRanges=np.array([bound.astuple() for bound in bounds]),
+            alpha_lvl_set=0.05,
+            eta0=0.25,
+            eta1=0.75,
+            delta=0.75,
+            gamma=1.25,
+            eps_tr=0.01,
+            min_tr_size=10.0,
+            TR_threshold=0.05,
+            test_fn=_test_fn,
+            gpr_model=InternalGPR(),
+            seed=seed,
+            local_search="gp_local_search",
+            behavior=Behavior.COVERAGE,
+        )
+        
+        return None
