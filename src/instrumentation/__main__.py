e4c7c3953210416cffc46b3d1b919fceae5255c2
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-06-12 18:58:11 -0700

    Create main method in instrumentation entrypoint

--- src/instrumentation/__main__.py
+++ src/instrumentation/__main__.py
@@ -5,30 +5,26 @@ from .model import ThermostatModel
 from .optimizer import SOAR, UniformRandom
 from .specification import ThermostatSpecification
 
-spec = ThermostatSpecification()
-model = ThermostatModel()
-options = Options(
-    static_parameters=[
-        (19.0, 22.0),
-        (19.0, 22.0),
-        (19.0, 22.0),
-        (19.0, 22.0),
-        (0.0, 1.0),
-        (0.0, 1.0),
-        (0.0, 1.0),
-        (0.0, 1.0),
-    ],
-    iterations=1000,
-    runs=1,
-)
 
-result_ur = staliro(model, ThermostatSpecification(), UniformRandom(), options)
-run_ur = result_ur.runs[0]
+def main():
+    spec = ThermostatSpecification()
+    model = ThermostatModel()
+    options = Options(
+        static_parameters=[(19.0, 22.0), (19.0, 22.0), (0.0, 1.0), (0.0, 1.0)],
+        iterations=1000,
+        runs=1,
+    )
+
+    result_ur = staliro(model, ThermostatSpecification(), UniformRandom(), options)
+    run_ur = result_ur.runs[0]
+
+    result_soar = staliro(model, ThermostatSpecification(), SOAR(), options)
+    run_soar = result_soar.runs[0]
+
+    print(f"Function branches: {len(spec.kripke.states)}")
+    print(f"UR coverage achieved in: {len(run_ur.history)}")
+    print(f"SOAR coverage achieved in: {len(run_soar.history)}")
 
 
-result_soar = staliro(model, ThermostatSpecification(), SOAR(), options)
-run_soar = result_soar.runs[0]
-
-print(f"Function branches: {len(spec.kripke.states)}")
-print(f"UR coverage achieved in: {len(run_ur.history)}")
-print(f"SOAR coverage achieved in: {len(run_soar.history)}")
+if __name__ == "__main__":
+    main()
