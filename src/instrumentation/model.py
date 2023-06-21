9e33e5e951b1711570ec3af025a6089fe3655ac6
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-06-20 19:50:19 -0700

    Create multiple models to support 2 room and 4 room scenarios

--- src/instrumentation/model.py
+++ src/instrumentation/model.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from bsa import instrument_function
 from staliro.core import Interval
 from staliro.core.model import BasicResult, Model, ModelInputs, ModelResult, Trace
 
-from thermostat import CoolingCooling, RoomParameters, State, SystemParameters, controller
+from thermostat import Controller, Cooling, RoomParameters, State, SystemParameters
 
 
 @dataclass
@@ -20,26 +21,27 @@ class InstrumentedOutput:
 _ModelResult = ModelResult[InstrumentedOutput, None]
 
 
-class ThermostatModel(Model[InstrumentedOutput, None]):
-    """Thermostat controller model.
+class ThermostatModel(Model[InstrumentedOutput, None], ABC):
+    """Thermostat controller model with 2 rooms.
 
     Simulate the thermostat model over the interval tspan. Create a trace of instrumented outputs
     containing both the state of the system and the conditional variables used during execution.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, controller: Controller) -> None:
         self.instr_fn = instrument_function(controller)
 
-    def simulate(self, inputs: ModelInputs, tspan: Interval) -> _ModelResult:
+    @abstractmethod
+    def setup(self, inputs: ModelInputs) -> tuple[State, SystemParameters]:
+        raise NotImplementedError()
+
+    def simulate(self, inputs: ModelInputs, interval: Interval) -> _ModelResult:
         t_step = 0.1
-        time = tspan.lower
-        state = CoolingCooling(inputs.static[0], inputs.static[1])
-        rm1_params = RoomParameters(heat=5.0, cool=inputs.static[2], bias=0.0)
-        rm2_params = RoomParameters(heat=5.0, cool=inputs.static[3], bias=0.0)
-        sys_params = SystemParameters([rm1_params, rm2_params], [(19.0, 22.0), (19.0, 22.0)])
+        time = interval.lower
+        state, sys_params = self.setup(inputs)
         timed_states: list[tuple[float, InstrumentedOutput]] = []
 
-        while time < tspan.upper:
+        while time < interval.upper:
             variables, state = self.instr_fn(state, t_step, sys_params)
             time = time + t_step
             timed_states.append((time, InstrumentedOutput(variables, state)))
@@ -49,3 +51,48 @@ class ThermostatModel(Model[InstrumentedOutput, None]):
         trace = Trace(times, states)
 
         return BasicResult(trace)
+
+
+class Thermostat2Rooms(ThermostatModel):
+    """Thermostat controller model with 2 rooms.
+
+    Simulate the thermostat model over the interval tspan. Create a trace of instrumented outputs
+    containing both the state of the system and the conditional variables used during execution.
+    """
+
+    def setup(self, inputs: ModelInputs) -> tuple[State, SystemParameters]:
+        state = Cooling(room1=inputs.static[0], room2=inputs.static[1], room3=0.0, room4=0.0)
+        bounds = (19.0, 22.0)
+        params = SystemParameters(
+            room1=RoomParameters(heat=5.0, cool=inputs.static[2], bias=0.0, bounds=bounds),
+            room2=RoomParameters(heat=5.0, cool=inputs.static[3], bias=0.0, bounds=bounds),
+            room3=RoomParameters(heat=0.0, cool=0.0, bias=0.0, bounds=bounds),
+            room4=RoomParameters(heat=0.0, cool=0.0, bias=0.0, bounds=bounds),
+        )
+
+        return state, params
+
+
+class Thermostat4Rooms(ThermostatModel):
+    """Thermostat controller model with 2 rooms.
+
+    Simulate the thermostat model over the interval tspan. Create a trace of instrumented outputs
+    containing both the state of the system and the conditional variables used during execution.
+    """
+
+    def setup(self, inputs: ModelInputs) -> tuple[State, SystemParameters]:
+        state = Cooling(
+            room1=inputs.static[0],
+            room2=inputs.static[1],
+            room3=inputs.static[2],
+            room4=inputs.static[3],
+        )
+        bounds = (19.0, 22.0)
+        params = SystemParameters(
+            room1=RoomParameters(heat=5.0, cool=inputs.static[4], bias=0.0, bounds=bounds),
+            room2=RoomParameters(heat=5.0, cool=inputs.static[5], bias=0.0, bounds=bounds),
+            room3=RoomParameters(heat=0.0, cool=inputs.static[6], bias=0.0, bounds=bounds),
+            room4=RoomParameters(heat=0.0, cool=inputs.static[7], bias=0.0, bounds=bounds),
+        )
+
+        return state, params
